5584b441fdf86755eb1ec94822e278be4401ff53
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-02 16:18:15 +0200

    task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in map(chr, range(97, 123)):
-    if letter != "e" or letter != "q":
+    if letter != "e" and letter != "q":
         print("{}".format(letter), end="")
