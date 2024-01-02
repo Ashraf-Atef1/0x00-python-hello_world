bd81a4e6ebcbc888885c5d35264ac048fdf01b7f
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-02 19:15:20 +0200

    task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for letter in map(chr, reversed(list(range(97, 123)))):
-    print("{}".format(letter), end="")
+for letter in reversed(list(range(97, 123))):
+    print("{}".format(chr(letter if not letter % 2 else letter - 32)), end="")
