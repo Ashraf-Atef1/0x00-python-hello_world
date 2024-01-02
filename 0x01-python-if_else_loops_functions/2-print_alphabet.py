3916c9509f2f9bb48e087b6cda38add685df736f
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-02 16:16:18 +0200

    task

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for letter in map(chr, range(97, 123)):
-    print("{}".format(letter), end="")
+    if letter == "e" or letter == "q":
+        print("{}".format(letter), end="")
