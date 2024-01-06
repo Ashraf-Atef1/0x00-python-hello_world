8ccf077c62e48cca6bca7436b520b43ace76fa05
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-06 15:12:01 +0200

    task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for l in matrix:
-        line = " ".join(map(str,l))
-        print("{:s}".format(line))
+    i = 0
+    for array in matrix:
+        while i < len(array):
+            if i == len(array) - 1:
+                print("{:d}".format(array[i]))
+            else:
+                print("{:d}".format(array[i]), end=" ")
+            i += 1
+        i = 0
