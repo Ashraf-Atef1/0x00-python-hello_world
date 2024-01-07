2df57648ade0633a3ba70373562d9a8b32793de4
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-07 21:53:39 +0200

    task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,23 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * is_palindrome_helper - fuction checks linked list is a palindrome or not
+ * @head: linked list to check
+ * @c_list: current node
+ * @is_palindrome: the value to return
+ * Return: return 1 if palindrome and 0 if not
+ */
+int is_palindrome_helper(listint_t **head, listint_t *c_list, int is_palindrome)
+{
+	if (!c_list)
+		return (is_palindrome);
+	is_palindrome_helper(head, c_list->next, is_palindrome);
+	if ((*head)->n != c_list->n)
+		is_palindrome = 0;
+	*head = (*head)->next;
+	return (is_palindrome);
+}
 /**
  * is_palindrome - fuction checks linked list is a palindrome or not
  * @head: linked list to check
@@ -9,32 +25,5 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int *list = malloc(sizeof(int));
-	int size = 0, i = 0;
-	listint_t *c_list = *head;
-
-	while (c_list)
-	{
-		c_list = c_list->next;
-		size++;
-	}
-	c_list = *head;
-	list = malloc(sizeof(int) * size);
-	while (c_list)
-	{
-		list[i++] = c_list->n;
-		c_list = c_list->next;
-	}
-	i = 0;
-	while (i < (size - 1) / 2)
-	{
-		if (list[i] != list[size - 1 - i])
-		{
-			free(list);
-			return (0);
-		}
-		i++;
-	}
-	free(list);
-	return (1);
+	return (is_palindrome_helper(head, *head, 1));
 }
