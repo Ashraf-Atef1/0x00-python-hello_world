0eb4d99b348f0c17ec3edaa3d5373d5f1e6183b2
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-10 17:19:09 +0200

    task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_symbol = {"M": 1000, "D": 500, "C": 100, "L":50, "X": 10, "V": 5, "I": 1}
+    roman_symbol = {"M": 1000, "D": 500,
+                    "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     sum = 0
-    if roman_string and type(roman_string) == str:
+    if roman_string and roman_string is str:
         old_value = 0
         for char in roman_string:
             if old_value < roman_symbol[char]:
