e97106071b5762d26b224d511c883d225c3545a0
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-10 16:45:15 +0200

    task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@ def best_score(a_dictionary):
     max_value = (None, None)
     if a_dictionary:
         for key, value in a_dictionary.items():
-            if(not max_value[1] or max_value[1] < value):
+            if not max_value[1] or max_value[1] < value:
                 max_value = (key, value)
     return max_value[0]
