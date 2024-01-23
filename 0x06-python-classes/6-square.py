0964f1de4b5aa7179503bcb684fa4f4b13ffaf05
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-23 17:31:19 +0200

    task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,8 +17,10 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        if not isinstance(position, tuple) or len(position) != 2:
+        if not isinstance(position, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(position) != 2:
+            raise ValueError("position must be a tuple of 2 positive integers")
         self.__position = position
 
     def area(self):
