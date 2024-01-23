c51447a6fa4ed145f8c5ee507db98196a92f3db2
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-23 17:49:31 +0200

    task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,6 +43,20 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
+    def position(self):
+        """position getter"""
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        """position setter"""
+        if (not isinstance(position, tuple)
+        or len(position) != 2 or
+        not all(isinstance(num, int) and num >= 0 for num in position)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
     def my_print(self):
         """function that print the square by # chars"""
         size = self.__size
