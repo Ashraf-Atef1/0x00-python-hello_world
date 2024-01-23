bc9d9ac4d7e595e8984b68892b5e6a82d3c4664a
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-23 16:16:12 +0200

    task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
         """
             initalization function
         """
-        if type(size) != "int":
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
