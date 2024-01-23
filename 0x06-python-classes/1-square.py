c3ebf224ddf26aa5dc031fdf5447149203c30e43
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-23 15:25:06 +0200

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This module contain a Square with size privite instance attribute
+"""
+
 class Square:
+    """
+    A class with privte instance attribute
+    """
     def __init__(self, size):
+        """
+            initalization function
+        """
         self.__size = size
