67d68a3ee861ba02997d74ffcf7367c7afd4a136
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-30 00:53:22 +0200

    task

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
-"""Defines a Rectangle class."""
+#!/usr/bin/python
+"""0-rectangle.py: module containe empty Rectangle object"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Rectangle object"""
     pass
