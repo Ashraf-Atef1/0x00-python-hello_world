7e21a9c868bc9d9531a4ed56a36c406c13988df3
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-30 20:43:03 +0200

    task

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def magic_string(s="BestSchool"):
-    return "\n".join([((f"{s}, " * i or "") + s) for i in range(10)])
+s=[]
+def magic_string():
+    return s.append("BestSchool") or ", ".join(s)
