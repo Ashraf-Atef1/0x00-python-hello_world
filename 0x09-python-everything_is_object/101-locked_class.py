24310b9e39e099a4c1961cd343f85793f73c3d25
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-30 21:31:47 +0200

    task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """Locked class module"""
+
+
 class LockedClass:
-    """ Locked class"""
+    """Locked class"""
+
     def __setattr__(self, name, value):
+        if name != "first_name":
+            raise AttributeError(
+                f"'{self.__class__.__name__}' object has no attribute '{name}'"
+            )
+
+    def __getattribute__(self, name):
         if name != "first_name":
             raise AttributeError(
                 f"'{self.__class__.__name__}' object has no attribute '{name}'"
