ade1395a9e62aed3a417e90776cbb89997ef89e8
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-05 13:12:21 +0200

    task

--- 0x0A-python-inheritance/5-main.py
+++ 0x0A-python-inheritance/5-main.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-Square = __import__('5-square').Square
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-my_square = Square(3)
-my_square.my_print()
+bg = BaseGeometry()
 
-print("--")
-
-my_square.size = 10
-my_square.my_print()
-
-print("--")
-
-my_square.size = 0
-my_square.my_print()
-
-print("--")
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
