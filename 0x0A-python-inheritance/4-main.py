1bb5e725606627b8b01fcf5c8ec20c9a2102db71
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-05 13:09:12 +0200

    task

--- 0x0A-python-inheritance/4-main.py
+++ 0x0A-python-inheritance/4-main.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-Square = __import__('4-square').Square
+inherits_from = __import__('4-inherits_from').inherits_from
 
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
