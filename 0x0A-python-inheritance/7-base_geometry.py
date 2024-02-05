de36ffa8cc4fe47eea7393707a9835c7125bebe1
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-05 15:15:13 +0200

    task

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,11 +14,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validate the value"""
-        if value.__class__ !=  int:
+        if value.__class__ != int:
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("./tests/7-base_geometry.txt")
