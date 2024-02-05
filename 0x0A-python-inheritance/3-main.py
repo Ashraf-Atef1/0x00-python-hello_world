01967c72d9ba66b02a610c6b9689b05ec8154d3f
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-05 12:57:40 +0200

    task

--- 0x0A-python-inheritance/3-main.py
+++ 0x0A-python-inheritance/3-main.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-Square = __import__('3-square').Square
+is_kind_of_class = __import__('3-is_kind_of_class').is_kind_of_class
 
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
+a = 1
+if is_kind_of_class(a, int):
+    print("{} comes from {}".format(a, int.__name__))
+if is_kind_of_class(a, float):
+    print("{} comes from {}".format(a, float.__name__))
+if is_kind_of_class(a, object):
+    print("{} comes from {}".format(a, object.__name__))
