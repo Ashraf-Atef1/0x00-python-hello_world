b707686d44d57b4f04f7f650a58215191806bb96
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-05 15:39:36 +0200

    task

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,3 +15,11 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """return the rectangle area"""
+        return self.__width*self.__height
+
+    def __str__(self):
+        """object string representation"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
