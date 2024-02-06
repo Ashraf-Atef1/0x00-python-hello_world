1980f350072b7a5785a3544ca216549a25175710
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-06 13:59:51 +0200

    task

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contain a class Student that defines a student
+"""
+
+
+class Student:
+    """a class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """initalization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """convert instance into a json object"""
+        my_dict = {}
+        for key, value in self.__dict__.items():
+            if not attrs or key in attrs:
+                my_dict[key] = value
+        return my_dict
