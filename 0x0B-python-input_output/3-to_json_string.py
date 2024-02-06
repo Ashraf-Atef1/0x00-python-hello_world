a2eefb1dc283f28a2dde649cc255d7d90d5f2a9b
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-06 12:56:00 +0200

    task

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -11,7 +11,4 @@ def to_json_string(my_obj):
     This module has a function that
     returns the JSON representation of an object (string)
     """
-    try:
-        return json.dumps(my_obj)
-    except TypeError:
-        raise TypeError(f"{my_obj} is not JSON serializable")
+    return json.dumps(my_obj)
