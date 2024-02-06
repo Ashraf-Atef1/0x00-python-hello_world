4076b89dd20f750f7b437f885c2d321684927742
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-06 12:32:42 +0200

    task

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module has a function that append a string to a text file (UTF8)
+and returns the number of characters appended
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    A function that append a string to a text file (UTF8)
+    and returns the number of characters appended
+    """
+    with open(filename, "at") as f:
+        return f.write(text)
