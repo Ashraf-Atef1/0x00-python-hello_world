b931e0197c18a1eca306e54cdc8fdcae8e1ce0cf
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-06 14:14:56 +0200

    task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contain a class Student that defines a student
+"""
+import json as js
+
+
+class Student:
+    """a class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """initalization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """convert instance into a json object"""
+        my_dict = {}
+        for key, value in self.__dict__.items():
+            if not isinstance(attrs, list) or key in attrs:
+                my_dict[key] = value
+        return my_dict
+    def reload_from_json(self, json):
+        """update instance attrbutes from a json file"""
+        with open(json, "r") as f:
+            my_dict = js.load(f)
+            for key, value in my_dict.items():
+                setattr(self, key, value)
