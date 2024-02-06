1b62438953f5e6a1baeadd1dc6fbddf06719aff9
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-06 12:30:15 +0200

    task

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module has a function that writes a string to a text file (UTF8)
+and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    A function that writes a string to a text file (UTF8)
+    and returns the number of characters written
+    """
+    with open(filename, "wt") as f:
+        f.write(text)
