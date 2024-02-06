b286df2dffaded3af8e85556684648be353553f1
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-06 12:27:24 +0200

    task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ and prints it to stdout
 
 def read_file(filename=""):
     """a function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, "r") as f:
-        print(f.read())
+    with open(filename, "rt") as f:
+        print(f.read(), end="")
