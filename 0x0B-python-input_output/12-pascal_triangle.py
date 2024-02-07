5ca4539f1fd95b5f4dd491ea406b2b1f54e6ca4f
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-07 14:30:27 +0200

    task

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -16,16 +16,14 @@ def pascal_triangle(n):
         return triangle_list[: n + 1]
     for level in range(3, n + 1):
         level_list = [1, level]
-        for number in range(1, old_mid):
-            level_list.append(
-                triangle_list[level - 1][number] +
-                triangle_list[level - 1][number + 1]
-            )
+        ol_list = triangle_list[level - 1]
+        level_list.extend([ol_list[i] + ol_list[i + 1]
+                           for i in range(1, old_mid)])
         if level % 2:
             new_mid = level_list[-1] * 2
-            level_list = level_list + level_list[::-1]
+            level_list = [*level_list, *level_list[::-1]]
         else:
             old_mid += 1
-            level_list = level_list + [new_mid] + level_list[::-1]
+            level_list = [*level_list, new_mid, *level_list[::-1]]
         triangle_list.append(level_list)
     return triangle_list
