6d14433cf654c765ee6a8d7d9efbcec1d91a3c79
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-12 20:02:08 +0200

    task

--- 0x0C-python-almost_a_circle/11-main.py
+++ 0x0C-python-almost_a_circle/11-main.py
@@ -4,7 +4,8 @@ from models.square import Square
 
 if __name__ == "__main__":
 
-    s = Square(10, 10, 10, 10)
-    s.update(id=None, size=7, x=18)
-    correct = "[Square] ({}) 18/10 - 7".format(s.id)
-    print(correct, str(s))
+    s = Square(7, 0, 0, [4])
+    s.size = 15
+    s.x = 8
+    s.y = 10
+    print("[Square] ([4]) 8/10 - 15", str(s))
