617674134de06b09e4468e5907a9d4cca0a9a1d5
Ashraf Atef <ashraf.programer@gmail.com>
2024-03-13 16:50:07 +0200

    task

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANT FOR user_0d_1@localhost;
-SHOW GRANT FOR user_0d_2@localhost;
+SHOW GRANTS user_0d_1@localhost, user_0d_2@localhost;
