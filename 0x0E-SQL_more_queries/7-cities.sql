d9d3ce2affa8af0690e9d728e09ef199749ee04c
Ashraf-Atef1 <144985320+Ashraf-Atef1@users.noreply.github.com>
2024-03-13 21:01:18 +0200

    Create 7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- a script that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
+CREATE TABLE IF NOT EXIST hbtn_0d_usa.cities(
+  id INT UNIQUE AUTO-INCREMENT NOT NULL PRIMARY KEY,
+  state_id INT FOREIGN KEY REFERENCES states.id,
+  name VARCHAR(256) NOT NULL
+);
