1280c69c3d0b95b3304508db52d55a53c26e7ce8
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-08 12:12:43 +0200

    task

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const first = Math.floor(+process.argv[2]);
 const second = Math.floor(+process.argv[3]);
-function add(a, b){
-	console.log(a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 add(first, second);
