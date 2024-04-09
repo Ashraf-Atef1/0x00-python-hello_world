3be1b7d52be48ef94bdae849047fd427e6e5d83b
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-09 13:48:20 +0200

    task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,19 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, firstFileData) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  fs.readFile(process.argv[3], 'utf-8', (err, secondFileData) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    fs.writeFile(process.argv[4], firstFileData + secondFileData, err => {
-      if (err) {
-        console.error(err);
-      }
-    });
-  });
-});
+
+const firstFileContent = fs.readFileSync(process.argv[2]).toString();
+const secondFileContent = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], firstFileContent + secondFileContent);
