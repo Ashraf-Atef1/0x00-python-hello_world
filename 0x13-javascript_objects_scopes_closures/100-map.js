4f2bbf89aa2047130e32985d2cbe8b22e3b06d50
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-09 13:15:41 +0200

    task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 const data = require('./100-data').list;
-let oldNum = 0;
-const newData = data.map((e) => {
-  const tmp = oldNum;
-  oldNum = e;
-  return e * tmp;
-});
 console.log(data);
-console.log(newData);
+console.log(data.map((e, idx) => e * idx));
