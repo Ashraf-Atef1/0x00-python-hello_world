a513579ba2b1aa285f30b19f6b8ef8646acaa72c
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-26 14:31:17 +0300

    task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""finds a peak in a list of unsorted integers"""
+
+
 def find_peak(list_of_integers):
     """ Function that finds a peak in a list of unsorted integers """
     if list_of_integers:
