c263f9f55d0cc356dbeefa2e15fb38c6ca4d4bbf
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-26 14:32:39 +0300

    task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,15 +4,6 @@
 
 def find_peak(list_of_integers):
     """ Function that finds a peak in a list of unsorted integers """
-    if list_of_integers:
-        pre_number = None
-        for i in range(len(list_of_integers) - 1):
-            current_number = list_of_integers[i]
-            next_number = list_of_integers[i + 1]
-            if (pre_number is not None and current_number > pre_number)\
-            and current_number > next_number:
-                return current_number
-            pre_number = current_number
-        return pre_number
-    else:
+    if len(list_of_integers) == 0:
         return None
+    return max(list_of_integers)
