cbd3723d56c4ee10b7d6a62803d90a12950eeafd
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-27 02:51:31 +0300

    task

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1"
+#!/bin/bash
+# prints the body of the repsonse of requests whose reposnse status is 200.
+RESPONSE=$(curl -sI "$1" | grep -E " {1}[0-9]{3} {1}"  | cut -d " " -f 2);if [ $RESPONSE == "200" ]; then curl -sX GET "$1"; fi
