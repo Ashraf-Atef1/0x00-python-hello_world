df209d91c80e8959d467440fc79d8cac450590f7
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-27 16:19:21 +0300

    task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,12 @@
 """Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)."""
 from sys import argv
-import urllib
-
+import urllib.request
+import urllib.error
 
 if __name__ == "__main__":
-      try:
-         with urllib.request.urlopen(argv[1]) as res:
-            print(res.read().decode("utf-8"))
-      except urllib.error.HTTPError as error:
-         print("Error code:", error.code)
+   try:
+      with urllib.request.urlopen(argv[1]) as res:
+         print(res.read().decode("utf-8"))
+   except urllib.error.HTTPError as error:
+      print(f"Error code: {error.code}")
