2fcadc79047cd18c0e9653183ae8c097a371afe4
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-21 18:59:08 +0300

    task 7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,17 +3,17 @@ const request = require('request');
 const URL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 let def = {};
 
-request(URL, function (err, res, body) {
+request(URL, function (err, res) {
   if (err) {
     console.log(err);
   } else {
-    const filmResult = JSON.parse(body);
-    filmResult.characters.forEach(function (item, index, array) {
-      request(item, function (err, res, content) {
+    const filmResult = JSON.parse(res.body);
+    filmResult.characters.forEach(item => {
+      request(item, function (err, res) {
         if (err) {
           console.log(err);
         } else {
-          def = JSON.parse(content);
+          def = JSON.parse(res.body);
           console.log(def.name);
         }
       });
