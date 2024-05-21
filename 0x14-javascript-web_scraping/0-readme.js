98a2205e4dc9fbcc8379f7d7cf8b39d40d470f92
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-21 14:52:38 +0300

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,10 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', (err) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
+  } else {
+    console.log(data);
   }
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const writeData = process.argv[3];
 
-fs.writeFile(filePath, writeData, 'utf8', (err, data) => {
+fs.writeFile(filePath, writeData, 'utf8', (err) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
   }
 });
