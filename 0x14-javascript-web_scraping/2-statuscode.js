f43e09bc35f8789f928e749d5f3b965d50203732
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-21 15:02:25 +0300

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@ const request = require('request');
 const URL = process.argv[2];
 
 request.get(URL, (err, res) => {
-  console.log(`code: ${res.statusCode}`)
-})
+  console.log(`code: ${res.statusCode}`);
+  if (err) {
+    console.log(err);
+  }
+});
