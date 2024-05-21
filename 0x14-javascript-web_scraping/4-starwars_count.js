a9e18d2457cd5a553b312ae5c312427e58195a52
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-21 17:29:27 +0300

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,8 +4,11 @@ const URL = process.argv[2];
 const ID = 18;
 
 request.get(URL, (err, res, body) => {
+  if (res.statusCode >= 300)
+    return console.log(0);
   const results = JSON.parse(body).results
-    .filter(film => film.characters
+  if (results)
+    results.filter(film => film.characters
       .includes(`https://swapi-api.alx-tools.com/api/people/${ID}/`)).length;
   console.log(results);
   if (err) {
