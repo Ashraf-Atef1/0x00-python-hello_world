1f09cb905aeba1d72059d6d7e074e1776316b71e
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-21 15:10:11 +0300

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 const ID = process.argv[2];
 
 request.get(`https://swapi-api.alx-tools.com/api/films/${ID}`, (err, res, body) => {
-  console.log(JSON.parse(body)["title"]);
+  console.log(JSON.parse(body).title);
   if (err) {
     console.log(err);
   }
