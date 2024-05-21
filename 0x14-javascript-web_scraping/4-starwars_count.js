c0dd2ced9e0cb223f9138b52aaf0f6586cfe970d
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-21 18:16:50 +0300

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,8 +6,8 @@ const ID = 18;
 request.get(URL, (err, res, body) => {
   try {
     const results = JSON.parse(body).results
-      .filter(film => film.characters.filter(e=> e.endsWith(`/${ID}/`))).length;
-    console.log(results);
+      .filter(film => film.characters.filter(e=> e.endsWith(`/${ID}/`)).length);
+    console.log(results.length);
   } catch {
     console.log(0);
   }
